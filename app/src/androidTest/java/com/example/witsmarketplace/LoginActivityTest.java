ac552dc7dbfad1a3bac74aae9f612dbf530154fb
XavierCharlie <makgakgacharles@gmail.com>
2021-09-07 14:52:02 +0000

    build testing

--- app/src/androidTest/java/com/example/witsmarketplace/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/witsmarketplace/LoginActivityTest.java
@@ -39,48 +39,10 @@ public class LoginActivityTest {
         mActivity = mActivityTestRule.getActivity();
     }
 
-
-
-    @Test
-    public void checkToast_Password_Only(){
-        Espresso.onView(withId(R.id.etPassword)).perform(typeText("123")); // same as above function but for password
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(withId(R.id.login_btn)).perform(click());
-        onView(withText("Incomplete fields")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
-
-    }
-    @Test
-    public void checkToast_Username_Only(){
-        Espresso.onView(withId(R.id.etUsername)).perform(typeText("vince@gmail.com"));//type in username field
-        Espresso.closeSoftKeyboard(); // close keyboard
-        Espresso.onView(withId(R.id.login_btn)).perform(click()); // press login btn
-        onView(withText("Incomplete fields")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));// check that Toast shows message
-
-    }
-
-    @Test
-    public  void checkToast_wrong_details(){
-        Espresso.onView(withId(R.id.etUsername)).perform(typeText("ddd"));
-        Espresso.onView(withId(R.id.etPassword)).perform(typeText("123"));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(withId(R.id.login_btn)).perform(click());
-        onView(withText(wrongDetails)).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public  void checkToast_correct_details(){
-        Espresso.onView(withId(R.id.etUsername)).perform(typeText("vince@gmail.com"));
-        Espresso.onView(withId(R.id.etPassword)).perform(typeText("123"));
-        Espresso.closeSoftKeyboard();
-        Espresso.onView(withId(R.id.login_btn)).perform(click());
-        onView(withText(correctDetails)).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
-    }
-
     @Test
     public void checkUsernameField()
     {
         EditText etUsername = mActivity.findViewById(R.id.etUsername);
-        // for Sitlabutla
         assertNotNull(etUsername); // this checks that the username field is not empty (has hint)
 
     }

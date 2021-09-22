c92953a483fb1f83afd535f916cf154d920d8e35
NihalSeegobin <1825548@student.wits.ac.za>
2021-09-22 22:47:45 +0200

    Added floating buttons to modal. Changed label+app_icon

--- app/src/main/java/com/example/witsmarketplace/LandingPage/modal.java
+++ app/src/main/java/com/example/witsmarketplace/LandingPage/modal.java
@@ -1,14 +1,18 @@
 package com.example.witsmarketplace.LandingPage;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.witsmarketplace.R;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +22,15 @@ public class modal extends AppCompatActivity
     private static String name;
     private static String price;
     private static String description;
+    private static ExtendedFloatingActionButton cartBtn;
+    private static ExtendedFloatingActionButton faveBtn;
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modal);
+        context = this;
 
         TextView name_text = findViewById(R.id.name);
         TextView price_text = findViewById(R.id.price);
@@ -36,6 +44,23 @@ public class modal extends AppCompatActivity
         name_text.setText(name);
         price_text.setText(price);
         desc_text.setText(description);
+
+        cartBtn = (ExtendedFloatingActionButton) findViewById(R.id.addCartBtn);
+        faveBtn = (ExtendedFloatingActionButton) findViewById(R.id.addFaveBtn);
+
+        cartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Added to Cart",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        faveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Added to Favorites",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ImageSlider imageSlider = findViewById(R.id.modal_image_slider);
         List<SlideModel> slideModels = new ArrayList<>();

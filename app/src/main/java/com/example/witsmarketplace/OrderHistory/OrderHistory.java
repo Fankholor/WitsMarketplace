a0e35c580ffb80b82087eff02adf71f6cc582ee4
charles <makgakgacharles@gmai.com>
2021-10-03 21:40:15 +0000

    OrderHistory Item

---
+++ app/src/main/java/com/example/witsmarketplace/OrderHistory/OrderHistory.java
@@ -0,0 +1,16 @@
+package com.example.witsmarketplace.OrderHistory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.witsmarketplace.R;
+
+public class OrderHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_history);
+    }
+}

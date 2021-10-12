cbd47f3eab542fa5ef452bd73b9ae2a9c05dd6e2
2185695 <2185695@students.wits.ac.za>
2021-10-12 07:29:33 +0000

    Account Activity

--- app/src/main/java/com/example/witsmarketplace/Account.java
+++ app/src/main/java/com/example/witsmarketplace/Account.java
@@ -2,7 +2,13 @@ package com.example.witsmarketplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.witsmarketplace.Login.LoginActivity;
+import com.example.witsmarketplace.OrderHistory.OrderHistory;
 
 public class Account extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class Account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        TextView orders = (TextView) findViewById(R.id.orders);
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OrderHistory.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }

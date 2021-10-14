b062a9155fb3699140def206ce975a12886520cb
charles <makgakgacharles@gmai.com>
2021-10-14 13:42:17 +0000

    PDF in progress

--- app/src/main/java/com/example/witsmarketplace/OrderHistory/Orders.java
+++ app/src/main/java/com/example/witsmarketplace/OrderHistory/Orders.java
@@ -4,21 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.witsmarketplace.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Orders extends AppCompatActivity {
     private static String date_str, street_name, surburb_name, city_name, country_name, items, order_no_str, total_str, email_str;
     private static String[] names;
     private static int[] prices;
+    private Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +48,7 @@ public class Orders extends AppCompatActivity {
         TextView order_no = findViewById(R.id.orderNo);
         TextView total = findViewById(R.id.totalPrice);
         TextView email = findViewById(R.id.email);
+        LinearLayout pdf = findViewById(R.id.pdf);
 
         Intent intent = getIntent();
         street_name = intent.getStringExtra("street");
@@ -61,8 +78,82 @@ public class Orders extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Invoice Downloaded", Toast.LENGTH_SHORT).show();
+                System.out.println("W: " + pdf.getWidth() + " H: " + pdf.getHeight());
+                int width = pdf.getWidth(), height = pdf.getHeight();
+                bitmap = loadBitmap(pdf, width, height);
+                createPDF();
             }
         });
+    }
+
+    private Bitmap loadBitmap(View pdf, int width, int height) {
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        pdf.draw(canvas);
+
+        return bitmap;
+    }
+
+    private void createPDF() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        //  Display display = wm.getDefaultDisplay();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        float height = displaymetrics.heightPixels;
+        float width = displaymetrics.widthPixels;
+
+        int convertHeight = (int) height, convertWidth = (int) width;
+
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(convertWidth, convertHeight, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        canvas.drawPaint(paint);
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, convertWidth, convertHeight, true);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        document.finishPage(page);
+
+        // write the document content
+        String targetPdf = "/sdcard/page.pdf";
+        File filePath;
+        filePath = new File(targetPdf);
+        try {
+            document.writeTo(new FileOutputStream(filePath));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
+        }////////////////////
+
+        // close the document
+        document.close();
+        Toast.makeText(this, "successfully pdf created", Toast.LENGTH_SHORT).show();
+
+        openPdf();
+
+    }
+
+    private void openPdf() {
+        File file = new File("/sdcard/page.pdf");
+        
+        if (file.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(file);
+            intent.setDataAndType(uri, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, "No Application for pdf view", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void renderer(){
